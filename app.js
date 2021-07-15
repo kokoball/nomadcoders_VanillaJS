f4c3dd488babacdcb617583aa797de4b3c0512da
unknown <kbp4154@naver.com>
2021-07-15 21:20:45 +0900

    7.15 4.2 Event 3:24

--- app.js
+++ app.js
@@ -8,4 +8,6 @@ function haddleMouseEnter() {
 }
 
 title.addEventListener("click", handleTitleClick); //() 안 붙이는 이유 자바스크립트가 직접실행
-title.addEventListener("mouseeneter", haddleMouseEnter)
+title.addEventListener("mouseeneter", haddleMouseEnter);
+
+TrackEvent
