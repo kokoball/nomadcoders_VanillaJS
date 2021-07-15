d16f3a4006141baf58b04e9a9071965a2b83c587
unknown <kbp4154@naver.com>
2021-07-15 14:10:35 +0900

    7.15

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-const title = document.querySelector(".hello h1");
+const title = document.querySelector("div.hello:first-child h1");
 
-console.log(title);
-addd
-s
+function handleTitleClick() {
+    title.style.color = "blue";
+}
+function haddleMouseEnter() {
+    console.log("mouse is here!")
+}
+
+title.addEventListener("click", handleTitleClick); //() 안 붙이는 이유 자바스크립트가 직접실행
+title.addEventListener("mouseeneter", haddleMouseEnter)
