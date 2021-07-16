a1b0db3f501c198548fb6dd05ae92b13abc9cc0b
unknown <kbp4154@naver.com>
2021-07-16 14:33:05 +0900

    7.16 4.4

--- app.js
+++ app.js
@@ -10,4 +10,22 @@ function haddleMouseEnter() {
 title.addEventListener("click", handleTitleClick); //() 안 붙이는 이유 자바스크립트가 직접실행
 title.addEventListener("mouseeneter", haddleMouseEnter);
 
-TrackEvent
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting");
+
+function onLoginBtnClick() {
+const HIDDEN_CLASSNAME = "hidden";
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  console.log(username);
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
